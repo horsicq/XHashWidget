4fa06fa6307dee52054178e1541eb26a16e66c34
horsicq <horsicq@gmail.com>
2021-08-28 00:55:40 +0200

    Fix: 2021-08-28

--- xhashwidget.cpp
+++ xhashwidget.cpp
@@ -194,7 +194,7 @@ void XHashWidget::registerShortcuts(bool bState)
 
 void XHashWidget::on_pushButtonSave_clicked()
 {
-    QString sFileName=XBinary::getResultFileName(g_pDevice,"hash.txt"); // TODO
+    QString sFileName=XBinary::getResultFileName(g_pDevice,QString("%1.txt").arg(tr("Hash"))); // TODO
 
     sFileName=QFileDialog::getSaveFileName(this,tr("Save"),sFileName,QString("%1 (*.txt);;%2 (*)").arg(tr("Text files"),tr("All files")));
 
