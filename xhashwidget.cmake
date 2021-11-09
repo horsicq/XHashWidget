41f91a1b962c0ef7f6e77fdc6fd5fa451d8bdd7f
horsicq <horsicq@gmail.com>
2021-11-09 19:41:06 +0100

    Fix: 2021-11-09

--- xhashwidget.cmake
+++ xhashwidget.cmake
@@ -3,11 +3,11 @@ include_directories(${CMAKE_CURRENT_LIST_DIR})
 # TODO
 
 set(XHASHWIDGET_SOURCES
+    ${CMAKE_CURRENT_LIST_DIR}/dialoghash.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialoghash.ui
     ${CMAKE_CURRENT_LIST_DIR}/dialoghashprocess.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialoghashprocess.ui
     ${CMAKE_CURRENT_LIST_DIR}/hashprocess.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xhashwidget.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/dialoghash.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/dialoghashprocess.ui
     ${CMAKE_CURRENT_LIST_DIR}/xhashwidget.ui
-    ${CMAKE_CURRENT_LIST_DIR}/dialoghash.ui
 )
